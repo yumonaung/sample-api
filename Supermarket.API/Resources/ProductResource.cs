d6221f35d2a13334b1792a4875933670624ce848
yumonaung <asm.yumonaung@gmail.com>
2020-09-03 11:41:49 +0900

    init
    init

---
+++ Supermarket.API/Resources/ProductResource.cs
@@ -0,0 +1,12 @@
+namespace Supermarket.API.Resources
+{
+    public class ProductResource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int QuantityInPackage { get; set; }
+        public string UnitOfMeasurement { get; set; }
+        public string Image { get; set; }
+        public CategoryResource Category {get;set;}
+    }
+}
